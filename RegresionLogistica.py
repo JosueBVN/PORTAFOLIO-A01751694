c68cc019fb8ddab3247f83d28da1448f3a51df08
JosueBVN <A01751694@tec.mx>
2023-08-28 13:15:22 -0600

    actualizacion 28-08-2023

--- RegresionLogistica.py
+++ RegresionLogistica.py
@@ -23,21 +23,22 @@ class LogisticRegression:
         self.weights = np.zeros(num_features)
         self.bias = 0
         
-        # Gradiente descendente
         for _ in range(self.num_iterations):
             linear_model = np.dot(X, self.weights) + self.bias
             predictions = self.sigmoid(linear_model)
             
+            # Gradientes
             dw = (1/num_samples) * np.dot(X.T, (predictions - y))
             db = (1/num_samples) * np.sum(predictions - y)
             
+            # Actualización de parámetros
             self.weights -= self.learning_rate * dw
             self.bias -= self.learning_rate * db
     
-    def predict(self, X):
+    def predict(self, X, threshold=0.5):
         linear_model = np.dot(X, self.weights) + self.bias
         predictions = self.sigmoid(linear_model)
-        binary_predictions = [1 if p >= 0.5 else 0 for p in predictions]
+        binary_predictions = [1 if p >= threshold else 0 for p in predictions]
         return binary_predictions
 
 # Datos de entrenamiento
@@ -45,10 +46,11 @@ X_train = np.array([[2.3, 4.5], [1.5, 3.2], [3.9, 7.5], [2.5, 5.8]])
 y_train = np.array([0, 0, 1, 1])
 
 # Crear modelo y entrenar
-model = LogisticRegression()
+model = LogisticRegression(learning_rate=0.01, num_iterations=1000)
 model.fit(X_train, y_train)
 
 # Datos de prueba
 X_test = np.array([[2.7, 5.3], [1.8, 3.7]])
 predictions = model.predict(X_test)
+
 print("Predictions:", predictions)
